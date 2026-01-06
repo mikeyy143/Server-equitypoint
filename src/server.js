71fc92e1243a91cbcad2549c7e14aa1b20bd6275
mike <azambaig.m3316@gmail.com>
2026-01-06 21:57:35 +0530

    Changed the backend strucutre for render

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
 import crypto from "crypto";
+import "dotenv/config";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 
 // counts
 const counts = {
